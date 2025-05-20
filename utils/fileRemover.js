9e687a2ab3094727c0d25eae3b63078fe2af85b8
VijayaSP <144252779+VijayaSP@users.noreply.github.com>
2025-05-20 19:37:25 +0530

    Update fileRemover.js

--- utils/fileRemover.js
+++ utils/fileRemover.js
@@ -27,12 +27,12 @@ const fileRemover = (filename) => {
   fs.unlink(path.join(__dirname, "../uploads", filename), function (err) {
     if (err && err.code == "ENOENT") {
       // file doesn't exist
-      console.log(File ${filename} doesn't exist, won't remove it.);
+      console.log(`File ${filename} doesn't exist, won't remove it.`);
     } else if (err) {
       console.log(err.message);
-      console.log(Error occured while trying to remove file ${filename});
+      console.log(`Error occured while trying to remove file ${filename}`);
     } else {
-      console.log(removed ${filename});
+      console.log(`removed ${filename}`);
     }
   });
 };
